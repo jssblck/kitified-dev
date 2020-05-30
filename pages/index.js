5008909d742180d7caa006982ab4814345a7b143
Kit Martin <kit@kitified.dev>
2020-05-30 12:24:45 -0700

    Remove title

--- pages/index.js
+++ pages/index.js
@@ -9,8 +9,6 @@ export default function Home() {
 
       <div class="max-w-lg mx-auto flex p-6 bg-white rounded-lg shadow-xl">
         <div class="ml-6 pt-1">
-          <h4 class="text-xl text-gray-900 leading-tight">kitified</h4>
-          <br/>
           <p class="text-base text-gray-600 leading-normal">
             Hi there! My name is Kit and I'm an engineer at <a href="https://fossa.com/">FOSSA</a>.
             If I'm not programming I'm probably playing games or reading.
